079380156bf4e8810c908f3fc4baba78fe3014ca
chandlerNick <njchandler2002@gmail.com>
2025-11-24 16:23:35 +0100

    Add standard Data

--- candidate-dataset-1/ds1.R
+++ candidate-dataset-1/ds1.R
@@ -1,18 +1,6 @@
-library(baseballr)
-library(dplyr)
-library(readr)
+data <- read.csv("standard.csv")
 
-bat_2020_2025 <- fg_batter_leaders(
-  startseason = "2020",
-  endseason   = "2025",
-  pos         = "all",
-  stats       = "bat",
-  lg          = "all",
-  qual        = "0",     # no PA minimum, get everyone
-  ind         = "1",     # return season-level rows, not aggregate
-  pageitems   = "10000", # avoid pagination
-  pagenum     = "1",
-)
+head(data)
 
-# Write to CSV
-write_csv(bat_2020_2025, "fangraphs_batters_2020_2025.csv")
+
+
--- tab2csv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-input="$1"
-output="$2"
-
-awk 'BEGIN {OFS=","} 
-     {for(i=1;i<=NF;i++) printf "%s%s", $i, (i==NF ? "\n" : OFS)}' FS='\t' "$input" > "$output"
-
